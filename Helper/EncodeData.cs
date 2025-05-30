785ed73bd4b830110d7a4fa8896d4ca264edcbf2
ZymboWannaCode <budiprasetiyo170@gmail.com>
2025-05-30 14:05:19 +0700

    Add project files.

---
+++ Helper/EncodeData.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace wms_back_end.Helper
+{
+    public class EncodeData
+    {
+        public static string[] HtmlEncodeObject(JObject data)
+        {
+            var encodedData = new List<string>();
+
+            foreach (var property in data.Properties())
+            {
+                encodedData.Add(WebUtility.HtmlEncode(property.Value.ToString()));
+            }
+
+            return encodedData.ToArray();
+        }
+    }
+}
---
+++ Helper/LDAPAuthentication.cs
@@ -0,0 +1,28 @@
+﻿using System.DirectoryServices;
+using System.Runtime.Versioning;
+
+namespace wms_back_end.Helper
+{
+    public class LDAPAuthentication(IConfiguration configuration)
+    {
+        readonly string adPath = configuration["Key:linkLDAP"]!;
+
+        [SupportedOSPlatform("windows")]
+        public bool IsAuthenticated(string username, string password)
+        {
+            return true;
+        }
+
+        [SupportedOSPlatform("windows")]
+        public string GetMail(string username)
+        {
+            return "";
+        }
+
+        [SupportedOSPlatform("windows")]
+        public string GetDisplayName(string username)
+        {
+            return username;
+        }
+    }
+}
